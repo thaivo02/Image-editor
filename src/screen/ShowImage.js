b4986ddb85406723bc356cc9aa18068930aac733
ThanhThai02 <20520305@gm.uit.edu.vn>
2023-02-19 04:20:29 +0700

    download and share image from cloud

--- src/screen/ShowImage.js
+++ src/screen/ShowImage.js
@@ -1,10 +1,26 @@
 import { Text, View, Image, Dimensions } from "react-native";
 import React, { Component } from "react";
 import { StatusBar } from "expo-status-bar";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { shareAsync } from "expo-sharing";
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
+import styles from "../stylesheet/Styles";
 
 const win = Dimensions.get("window");
 
 const ratio = win.width / 541; //541 is actual image width
+
+const Download = async (uri) => {
+  const downloadInstance = FileSystem.createDownloadResumable(
+    uri,
+    FileSystem.documentDirectory + "image.jpg"
+  );
+
+  const result = await FileSystem.downloadInstance.downloadAsync();
+
+  return result;
+};
 
 export class ShowImage extends Component {
   constructor(props) {
@@ -13,6 +29,30 @@ export class ShowImage extends Component {
       image: null,
     };
   }
+
+  sharePic = async () => {
+    const downloadInstance = FileSystem.createDownloadResumable(
+      this.props.route.params.image,
+      FileSystem.documentDirectory + "image.jpg"
+    );
+
+    const result = await downloadInstance.downloadAsync();
+
+    shareAsync(result.uri);
+  };
+
+  savePhoto = async () => {
+    const downloadInstance = FileSystem.createDownloadResumable(
+      this.props.route.params.image,
+      FileSystem.documentDirectory + "image.jpg"
+    );
+
+    const result = await downloadInstance.downloadAsync();
+
+    const asset = await MediaLibrary.createAssetAsync(result.uri);
+
+    alert("Image saved successfully to the media library.");
+  };
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center" }}>
@@ -26,6 +66,18 @@ export class ShowImage extends Component {
               }}
             />
           )}
+          <TouchableOpacity
+            onPress={this.savePhoto}
+            style={styles.SaveImageButton}
+          >
+            <Text>Download image</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={this.sharePic}
+            style={styles.SaveImageButton}
+          >
+            <Text>Share image</Text>
+          </TouchableOpacity>
         </View>
         <StatusBar style="auto" />
       </View>
